ac25c3ab94181c9f107e8e4a11b4920a9643f4d3
v2kovac <vk.kovac@gmail.com>
2015-12-01 16:26:02 -0800

    get contact photos and next page of contacts

--- gcontacts_api.ml
+++ gcontacts_api.ml
@@ -2,7 +2,6 @@ open Printf
 open Lwt
 open Log
 open Util_url.Op
-open Gcontacts_api_t
 
 module Http = Util_http_client.Wrap (Util_http_client.Original) (struct
   type orig_result = Util_http_client.response
@@ -17,10 +16,9 @@ let get_contacts with_token email =
   with_token (fun access_token ->
     let s_email = Email.to_string email in
     let path = "m8/feeds/contacts/" ^ Util_url.encode s_email
-               ^ "/full" in
+               ^ "/thin" in
     let headers = [Google_auth.auth_header access_token;
-                 "Content-Type", "application/json";
-                 "GData-Version", "3.0"] in
+                   "GData-Version", "3.0"] in
     let url = Google_api_util.make_uri
       ~host:"www.google.com"
       ~query:["alt",["json"]]
@@ -28,5 +26,45 @@ let get_contacts with_token email =
     in
     Http.get ~headers url
   ) >>= function
-  | (`OK, _headers, body) -> return body
+  | (`OK, _headers, body) ->
+      return (Gcontacts_api_j.gcontacts_resp_of_string body)
   | (status,headers,body) -> Google_http.fail "contacts" status body
+
+let get_contacts_next_page with_token string_url =
+  with_token (fun access_token ->
+    let url = Uri.of_string string_url in
+    let headers = [Google_auth.auth_header access_token;
+                   "GData-Version", "3.0"] in
+    Http.get ~headers url
+  ) >>= function
+  | (`OK, _headers, body) ->
+      return (Gcontacts_api_j.gcontacts_resp_of_string body)
+  | (status,headers,body) -> Google_http.fail "contacts" status body
+
+let get_photo with_token email contact_id =
+  with_token (fun access_token ->
+    let s_email = Email.to_string email in
+    let path = "m8/feeds/photos/media/" ^ Util_url.encode s_email
+               ^ Util_url.encode contact_id in
+    let headers = [Google_auth.auth_header access_token;
+                   "GData-Version", "3.0"] in
+    let url = Google_api_util.make_uri
+      ~host:"www.google.com"
+      path
+    in
+    Http.get ~headers url
+  ) >>= function
+  | (`OK, _headers, body) -> return body
+  | (status,headers,body) ->
+      Google_http.fail "contact photo" status body
+
+let get_photo_with_url with_token string_url =
+  with_token (fun access_token ->
+    let url = Uri.of_string string_url in
+    let headers = [Google_auth.auth_header access_token;
+                   "GData-Version", "3.0"] in
+    Http.get ~headers url
+  ) >>= function
+  | (`OK, _headers, body) -> return body
+  | (status,headers,body) ->
+      Google_http.fail "contact photo" status body
